6a2be83f4198bddf39ed7aaaaec19f153fc26437
d3bes <abdo.mohamed6319@gmail.com>
2024-06-23 02:19:42 +0300

    update IBaseRepository

--- src/LearnHub.Core/Interfaces/IBaseRepository.cs
+++ src/LearnHub.Core/Interfaces/IBaseRepository.cs
@@ -3,23 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
+using LearnHub.Core.Consts;
 
 namespace LearnHub.Core.Interfaces
 {
     public interface IBaseRepository<TEntity>  where TEntity : class
     {
 
+/// <summary>
+/// Synchronise methodes
+/// </summary>
         TEntity getById(int  id );
-        Task<TEntity> getByIdAsync(int id);
 
-        Task<List<TEntity>> getAllAsync();
         List<TEntity> getAll();
-        Task<TEntity> AddAsync(TEntity entity);
         TEntity Add(TEntity entity);
 
         TEntity find(Expression<Func<TEntity,bool>> expression);
+        TEntity find(Expression<Func<TEntity,bool>> expression, string[] includs = null);
+
+
+
+         IEnumerable<TEntity> findAll(Expression<Func<TEntity,bool>> expression);
+       
+        IEnumerable<TEntity> findAll(Expression<Func<TEntity,bool>> expression, string[] includs = null);
+        IEnumerable<TEntity> findAll(Expression<Func<TEntity,bool>> expression, int take, int skip , string[] includs = null);
+
+        IEnumerable<TEntity> findAll(Expression<Func<TEntity, bool>> criteria, int? take, int? skip, 
+            Expression<Func<TEntity, object>> orderBy = null, string orderByDirection = OrderBy.Ascending);
+    TEntity  add(TEntity entity);
+    IEnumerable<TEntity>  addRange(IEnumerable<TEntity> entities);
+/// <summary>
+/// Asynchronise methodes
+/// </summary>
+       
+        Task<TEntity> getByIdAsync(int id);
+        Task<List<TEntity>> getAllAsync();
+        Task<TEntity> AddAsync(TEntity entity);
+         Task<TEntity> findAsync(Expression<Func<TEntity,bool>> expression);
+        Task<TEntity> findAsync(Expression<Func<TEntity,bool>> expression, string[] includs = null);
+       Task<IEnumerable<TEntity>> findAllAsync(Expression<Func<TEntity,bool>> expression);
+        Task<IEnumerable<TEntity>> findAllAsync(Expression<Func<TEntity,bool>> expression, string[] includs = null);
+         Task<IEnumerable<TEntity>> findAllAsync(Expression<Func<TEntity,bool>> expression, int take, int skip, string[] includs = null);
+
+        Task<IEnumerable<TEntity>> findAllAsync(Expression<Func<TEntity, bool>> criteria, int? skip, int? take,
+            Expression<Func<TEntity, object>> orderBy = null, string orderByDirection = OrderBy.Ascending);
+
+
+    Task<TEntity>  addAsync(TEntity entity);
+
+    Task<IEnumerable<TEntity>>  addRangeAsync(IEnumerable<TEntity> entities);
 
         // Task<TEntity> UpdateAsync(TEntity entity);
         
     }
+    
 }
