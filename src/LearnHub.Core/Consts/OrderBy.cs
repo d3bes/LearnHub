760dd583d704a2b8c46ecd4ffead600b3ca3400a
d3bes <abdo.mohamed6319@gmail.com>
2024-06-23 02:16:34 +0300

    add OrderBy

---
+++ src/LearnHub.Core/Consts/OrderBy.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnHub.Core.Consts
+{
+    public static class OrderBy
+    {
+        public const string Ascending = "ASC";
+public const string Descending = "DESC";
+
+    }
+}
