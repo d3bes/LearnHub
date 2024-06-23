4fcc757695e54dca9d9fe70d2c0b087548589486
d3bes <abdo.mohamed6319@gmail.com>
2024-06-24 02:40:35 +0300

    Add Enrollmrnt migration

---
+++ src/LearnHub.EF/Migrations/20240623184025_AddEnrollment.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LearnHub.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEnrollment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "enrollments");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "enrollments",
+                columns: table => new
+                {
+                    EnrollmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    courseId = table.Column<int>(type: "int", nullable: false),
+                    studentId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    enrollmentDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    studintId = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_enrollments", x => x.EnrollmentId);
+                    table.ForeignKey(
+                        name: "FK_enrollments_AspNetUsers_studentId",
+                        column: x => x.studentId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_enrollments_courses_courseId",
+                        column: x => x.courseId,
+                        principalTable: "courses",
+                        principalColumn: "CourseId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_enrollments_courseId",
+                table: "enrollments",
+                column: "courseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_enrollments_studentId",
+                table: "enrollments",
+                column: "studentId");
+        }
+    }
+}
