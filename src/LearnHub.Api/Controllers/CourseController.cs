9e4f006e3fd21d59a0894c2994a09ad13f3a86f3
d3bes <abdo.mohamed6319@gmail.com>
2024-06-23 22:10:48 +0300

    update CourseController

--- src/LearnHub.Api/Controllers/CourseController.cs
+++ src/LearnHub.Api/Controllers/CourseController.cs
@@ -5,26 +5,89 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using LearnHub.Core.Interfaces;
 using LearnHub.Core.Models;
+using LearnHub.Core.Dto;
 
 namespace LearnHub.Api.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class CourseController : ControllerBase
-    {
-     private readonly IBaseRepository<Course> _courseRepository;   
+   [ApiController]
+   [Route("api/[controller]")]
+   public class CourseController : ControllerBase
+   {
+      private readonly IBaseRepository<Course> _courseRepository;
+      private readonly IBaseRepository<User> _instructionRepository;
 
-     public CourseController(IBaseRepository<Course> courseRepository)
-     {
-        _courseRepository = courseRepository;
-     }
 
-     [HttpGet]
-     public IActionResult  GetAllCourses()
-     {
-        
-        return Ok(_courseRepository.getAllAsync());
+      public CourseController(IBaseRepository<Course> courseRepository, IBaseRepository<User> instructionRepository)
+      {
+         _courseRepository = courseRepository;
+         _instructionRepository = instructionRepository;
 
-     }
-    }
+      }
+
+      [HttpGet("GetAllCourses")]
+      public async Task<IActionResult> GetAllCourses()
+      {
+
+         var courses = await _courseRepository.getAllAsync(["Instructor"]);
+         return Ok(courses);
+
+      }
+
+      [HttpGet("GetCourseById")]
+      public async Task<IActionResult> GetCourseById(int id)
+      {
+         if (id != null)
+         {
+            var result = await _courseRepository.getByIdAsync(id);
+            if (result != null)
+            {
+               return Ok(result);
+            }
+            else
+               return NotFound();
+         }
+         return NoContent();
+      }
+
+      [HttpPost("AddCourse")]
+      public async Task<IActionResult> AddCourse(Course course)
+      {
+         string instructorId = course.InstructorId;
+         var instructor = await _instructionRepository.findAsync(x => x.Id == instructorId);
+         course.Instructor = instructor;
+         var result = await _courseRepository.AddAsync(course);
+         return Ok(result);
+      }
+
+      [HttpPost("updateCourse")]
+      public IActionResult UpdateCourse(CourseDto courseDto)
+      {
+
+         var course = _courseRepository.getById(courseDto.CourseId);
+         course.Description = courseDto.Description;
+         course.InstructorId = courseDto.InstructorId;
+         course.Title = courseDto.Title;
+
+         //var newInstructor = await _instructionRepository.findAsync(x=> x.Id == courseDto.InstructorId);
+         var result = _courseRepository.update(course);
+
+         return Ok(result);
+
+      }
+
+      [HttpDelete("deleteCourse")]
+      public IActionResult DeleteCourse(int id)
+      {
+         var course = _courseRepository.getById(id);
+         if (course != null)
+         {
+            _courseRepository.Delete(course);
+            return Ok(true);
+         }
+         else
+            return NotFound();
+
+      }
+
+   }
 }
