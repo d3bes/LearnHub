5d13512c6e02f663c5ed5459d12fbf21e9122b57
d3bes <abdo.mohamed6319@gmail.com>
2024-06-23 21:48:01 +0300

    add dto

---
+++ src/LearnHub.Core/Dto/CourseDto.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnHub.Core.Dto
+{
+    public class CourseDto
+    {
+    public int CourseId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+
+    // Foreign key to instructor (one-to-one with User)
+    public string InstructorId { get; set; }
+    }
+}
