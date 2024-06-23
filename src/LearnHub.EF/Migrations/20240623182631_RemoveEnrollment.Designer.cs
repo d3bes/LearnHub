f7a8654c75181888d989ce476a3681588032549e
d3bes <abdo.mohamed6319@gmail.com>
2024-06-23 21:47:21 +0300

    updaet enrollment migrations

--- src/LearnHub.EF/Migrations/20240623182631_RemoveEnrollment.Designer.cs
+++ src/LearnHub.EF/Migrations/20240623182631_RemoveEnrollment.Designer.cs
@@ -4,6 +4,7 @@ using LearnHub.EF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LearnHub.EF.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240623182631_RemoveEnrollment")]
+    partial class RemoveEnrollment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -106,7 +109,7 @@ namespace LearnHub.EF.Migrations
 
                     b.HasIndex("studentId");
 
-                    b.ToTable("enrollment");
+                    b.ToTable("enrollments");
                 });
 
             modelBuilder.Entity("LearnHub.Core.Models.Grade", b =>
---
+++ src/LearnHub.EF/Migrations/20240623184256_AddNewEnrollment.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LearnHub.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewEnrollment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "enrollment",
+                columns: table => new
+                {
+                    EnrollmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    enrollmentDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    studentId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    courseId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_enrollment", x => x.EnrollmentId);
+                    table.ForeignKey(
+                        name: "FK_enrollment_AspNetUsers_studentId",
+                        column: x => x.studentId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_enrollment_courses_courseId",
+                        column: x => x.courseId,
+                        principalTable: "courses",
+                        principalColumn: "CourseId",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_enrollment_courseId",
+                table: "enrollment",
+                column: "courseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_enrollment_studentId",
+                table: "enrollment",
+                column: "studentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "enrollment");
+        }
+    }
+}
