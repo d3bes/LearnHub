14f06528f31607613ee43c401c1a3247becc790c
d3bes <abdo.mohamed6319@gmail.com>
2024-06-23 22:10:06 +0300

    update course model

--- src/LearnHub.Core/Models/Course.cs
+++ src/LearnHub.Core/Models/Course.cs
@@ -14,10 +14,10 @@ namespace LearnHub.Core.Models
     public string Description { get; set; }
 
     // Foreign key to instructor (one-to-one with User)
-    public string InstructorId { get; set; }
-    public User Instructor { get; set; }
+    public string? InstructorId { get; set; }
+    public User? Instructor { get; set; }
 
-    public List<Module> Modules { get; set; }
+    public List<Module>? Modules { get; set; }
 
 
     }
