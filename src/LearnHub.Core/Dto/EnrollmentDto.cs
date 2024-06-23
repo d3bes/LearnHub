8077f7ad8c17058ca8e3080f3e3c8c2693bbbef1
d3bes <abdo.mohamed6319@gmail.com>
2024-06-24 02:39:39 +0300

    add EnrollmentDto

---
+++ src/LearnHub.Core/Dto/EnrollmentDto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnHub.Core.Dto
+{
+    public class EnrollmentDto
+    {
+        public int EnrollmentId { get; set; }
+        public string studentId { get; set; }
+        public int courseId { get; set; }
+
+    }
+}
