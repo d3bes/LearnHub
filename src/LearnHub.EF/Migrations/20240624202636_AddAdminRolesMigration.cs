871ae13cb6e67808da9535174ddce4970e4cfc9a
d3bes <abdo.mohamed6319@gmail.com>
2024-06-24 23:33:08 +0300

    add admin roles migration

---
+++ src/LearnHub.EF/Migrations/20240624202636_AddAdminRolesMigration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LearnHub.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdminRolesMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+                migrationBuilder.Sql("insert into [dbo].[AspNetUserRoles] ([UserId],[RoleId]) select '07a700e7-6f45-44bf-a4f1-76f5f754fe3f' , id from [dbo].[AspNetRoles]");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.Sql("delete from [dbo].[AspNetUserRoles] where [UserId] ='07a700e7-6f45-44bf-a4f1-76f5f754fe3f");
+        }
+    }
+}
