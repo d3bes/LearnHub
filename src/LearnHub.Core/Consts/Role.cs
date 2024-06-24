35585fb6785652386b7e5055e0c47b0140e6d28e
d3bes <abdo.mohamed6319@gmail.com>
2024-06-24 21:44:25 +0300

    add role cons

---
+++ src/LearnHub.Core/Consts/Role.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnHub.Core.Consts
+{
+    public class Role
+    {
+        public const string admin = "Administrator";
+        public const string instructor = "Instructor";
+        public const string student = "Student";
+        public const string assistant = "Assistant";
+    }
+}
