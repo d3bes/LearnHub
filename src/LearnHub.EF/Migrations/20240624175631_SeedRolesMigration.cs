19487b68cee9dd62fb9b5ff366d0eeb5dc1ded68
d3bes <abdo.mohamed6319@gmail.com>
2024-06-24 21:44:51 +0300

    add seed role migration

---
+++ src/LearnHub.EF/Migrations/20240624175631_SeedRolesMigration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore.Migrations;
+using LearnHub.Core.Consts;
+
+#nullable disable
+
+namespace LearnHub.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRolesMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+                values: new object[] { Guid.NewGuid().ToString(), Role.admin, Role.admin.ToUpper(), Guid.NewGuid().ToString() }
+            );
+
+            migrationBuilder.InsertData(
+              table: "AspNetRoles",
+              columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+              values: new object[] { Guid.NewGuid().ToString(), Role.instructor, Role.instructor.ToUpper(), Guid.NewGuid().ToString() }
+          );
+
+            migrationBuilder.InsertData(
+              table: "AspNetRoles",
+              columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+              values: new object[] { Guid.NewGuid().ToString(), Role.student, Role.student.ToUpper(), Guid.NewGuid().ToString() }
+          );
+            migrationBuilder.InsertData(
+               table: "AspNetRoles",
+               columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+               values: new object[] { Guid.NewGuid().ToString(), Role.assistant, Role.assistant.ToUpper(), Guid.NewGuid().ToString() }
+           );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("delete from AspNetRoles");
+        }
+    }
+}
