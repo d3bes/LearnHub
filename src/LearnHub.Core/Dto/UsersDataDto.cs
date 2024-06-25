c2b5f57215f632327d9c08d6c870c89ac1f6a0a3
d3bes <abdo.mohamed6319@gmail.com>
2024-06-26 02:26:54 +0300

    initial

---
+++ src/LearnHub.Core/Dto/UsersDataDto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnHub.Core.Dto
+{
+    public class UsersDataDto
+    {
+        public string Id { get; set;}
+        public string UserName { get; set;}
+        public string Email { get; set;}
+        
+
+    }
+}
