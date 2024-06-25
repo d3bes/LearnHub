4112dd56e172cf41c7827aaba7890b0410dba9a6
d3bes <abdo.mohamed6319@gmail.com>
2024-06-25 16:02:20 +0300

    addADminpassword

--- src/LearnHub.Api/Controllers/UserController.cs
+++ src/LearnHub.Api/Controllers/UserController.cs
@@ -2,7 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using LearnHub.EF.Migrations;
+using LearnHub.Core.Models;
+using LearnHub.Core.Dto;
+using Microsoft.Identity.Client;
+using LearnHub.Core.Interfaces;
 
 namespace LearnHub.Api.Controllers
 {
@@ -10,6 +16,63 @@ namespace LearnHub.Api.Controllers
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
-        
+        // private readonly RoleManager<IdentityUser> _roleManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ITokenService _tokenService;
+
+        public UserController(UserManager<IdentityUser> userManager, ITokenService tokenService)
+
+        {
+
+            // _roleManager = roleManager;
+
+            _userManager = userManager;
+            _tokenService = tokenService;
+
+        }
+        [HttpPost("addAdminPassword")]
+        public async Task<IActionResult> AddAdminPasswordForFirstTimeAsync(string password) //Admin_pwd1
+        {
+            string adminId = "07a700e7-6f45-44bf-a4f1-76f5f754fe3f";
+            IdentityUser user = await _userManager.FindByIdAsync(adminId);
+
+            if (user == null)
+            {
+                return BadRequest("Admin user not found.");
+            }
+
+            user = await _userManager.FindByIdAsync(adminId);
+
+            if (user.PasswordHash == null)
+            {
+                var result = await _userManager.AddPasswordAsync(user, password);
+
+                if (result.Succeeded)
+                {
+                    user = await _userManager.FindByIdAsync(adminId); // Refresh user data
+                    var token = _tokenService.CreateAdminToken(user, _userManager);
+
+                    return Ok(token); // Return the updated password hash
+
+                }
+                else
+                {
+                    return BadRequest($"Failed to set password for admin user: {result.Errors}");
+                }
+            }
+            else
+            {
+                return BadRequest("Admin user already has a password set.");
+            }
+        }
+
+
+
+
+
+
+
     }
+
+
 }
