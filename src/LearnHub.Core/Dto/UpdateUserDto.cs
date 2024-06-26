a6ad8158b718d5ae40cc94cdd4e26f89836c9304
d3bes <abdo.mohamed6319@gmail.com>
2024-06-27 02:07:57 +0300

    update UserDto

--- src/LearnHub.Core/Dto/UpdateUserDto.cs
+++ src/LearnHub.Core/Dto/UpdateUserDto.cs
@@ -7,9 +7,13 @@ namespace LearnHub.Core.Dto
 {
     public class UpdateUserDto
     {
-          public string Id { get; set; }
+        public string Id { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
-        public string PhoneNumber?  { get; set; }
+        public bool emailConfirmed { get; set; }
+        public string? phoneNumber { get; set; }
+        public bool? phoneNumberConfirmed { get; set; }
+
+
     }
 }
