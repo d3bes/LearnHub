a385608e3fbf0ed9ec1d9ed839b295c54ab044d1
d3bes <abdo.mohamed6319@gmail.com>
2024-06-27 02:20:15 +0300

    init

---
+++ src/LearnHub.Core/Dto/ResetPasswordDto.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnHub.Core.Dto
+{
+    public class ResetPasswordDto
+    {
+        public string NewPassword { get; set; }
+
+    }
+}
