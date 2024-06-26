63c732a82aa2df70d9cb27976df858cccd2d4433
d3bes <abdo.mohamed6319@gmail.com>
2024-06-27 02:20:30 +0300

    init

---
+++ src/LearnHub.Core/Dto/NotificationDto.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnHub.Core.Dto
+{
+    public class NotificationDto
+    {
+         public string Message { get; set; }
+        public string Subject { get; set; }
+    }
+}
