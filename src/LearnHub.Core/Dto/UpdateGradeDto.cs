4971a4719a1eaf915ff06fb0e8086d98a9bbccba
d3bes <abdo.mohamed6319@gmail.com>
2024-06-27 23:52:56 +0300

    init

---
+++ src/LearnHub.Core/Dto/UpdateGradeDto.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnHub.Core.Dto
+{
+    public class UpdateGradeDto
+    {
+        public int GradeId { get; set; }
+        public float Score { get; set; }
+    }
+}
