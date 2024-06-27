df2bef2d4e679934811737c43e79b8383ae1bada
d3bes <abdo.mohamed6319@gmail.com>
2024-06-28 00:43:28 +0300

    add DateTime and Name to Grade  migrations

---
+++ src/LearnHub.EF/Migrations/20240627211937_AddGradeName.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LearnHub.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGradeName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GradeName",
+                table: "grades",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GradeName",
+                table: "grades");
+        }
+    }
+}
