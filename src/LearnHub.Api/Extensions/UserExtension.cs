2a4f237006a18591e623e86f2cb707a59f664f5a
d3bes <abdo.mohamed6319@gmail.com>
2024-06-28 23:59:01 +0300

    add ToUserDto(IdentityUser) overload

--- src/LearnHub.Api/Extensions/UserExtension.cs
+++ src/LearnHub.Api/Extensions/UserExtension.cs
@@ -11,7 +11,20 @@ namespace LearnHub.Api.Extensions
 {
     public static class UserExtension
     {
-        public static UserDto ToUserDto(this IdentityUser user)
+        public static UserDto ToUserDto(this User user)
+        {
+            return new UserDto(){
+                    Id = user.Id,
+                    Name = user.Name,
+                    UserName = user.UserName,
+                    Email = user.Email,
+                    emailConfirmed =  user.EmailConfirmed,
+                    phoneNumber = user.PhoneNumber,
+                    phoneNumberConfirmed = user.PhoneNumberConfirmed
+
+            };
+        }
+         public static UserDto ToUserDto(this IdentityUser user)
         {
             return new UserDto(){
                     Id = user.Id,
@@ -24,7 +37,7 @@ namespace LearnHub.Api.Extensions
             };
         }
 
-        public static List<UserDto> ToUserDtoList(this IList<IdentityUser> users) 
+        public static List<UserDto> ToUserDtoList(this IList<User> users) 
         {
             List<UserDto> result = new List<UserDto>();
             foreach(var user in users)
