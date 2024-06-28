9a7ae210f1899e098cf78648633f099982eeafab
d3bes <abdo.mohamed6319@gmail.com>
2024-06-28 23:52:48 +0300

    add datetime

--- src/LearnHub.Core/Dto/GradeDto.cs
+++ src/LearnHub.Core/Dto/GradeDto.cs
@@ -10,10 +10,13 @@ namespace LearnHub.Core.Dto
         public int GradeId { get; set; }
         public float Score { get; set; }
         public int CourseId { get; set; }
-        public string CourseName { get; set; }
+        public string GradeName { get; set; }
+        public string? CourseName { get; set; }
         public string StudentId { get; set; }
-        public string StudentName { get; set; }
+        public string? StudentName { get; set; }
+        public DateTime? dateTime { get; set; }
 
-        
+
+
     }
 }
